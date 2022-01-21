8dd83e7fc8f90a0dd326e97feb4d7be715fe87a4
MOHAMED NUMAN NASEER <abcandmum@gmail.com>
2022-01-21 15:55:27 +0530

    2nd commit

--- src/config/google.config.js
+++ src/config/google.config.js
@@ -10,7 +10,7 @@ export default (passport) => {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL:
-          "https://zomato-master-server-a0821-b08.herokuapp.com/auth/google/callback` ",
+          "https://zomato--backend.herokuapp.com/auth/google/callback` ",
       },
       async (accessToken, refreshToken, profile, done) => {
         // create a new user object
